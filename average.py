28c38f220ed3849d0242a2da8a4062abd0ce7d0d
RakeshKumar625 <rakeshmahto625@gmail.com>
2024-09-03 00:10:46 +0530

    Doing Chapte 2 some basics questions

---
+++ average.py
@@ -0,0 +1,8 @@
+# Average of Three numbers
+x = int(input("Enter the first number : "))
+y = int(input("Enter the second number : "))
+z = int(input("Enter the third number : "))
+
+avg=(x+y+z)/3
+
+print(avg)
---
+++ sum2.py
@@ -0,0 +1,4 @@
+a = int(input("Enter The First Number : "))
+b = int(input("Enter The Second Number : "))
+sum = (a+b)
+print("The sum of the two numbers are ",sum)
